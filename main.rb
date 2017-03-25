3a52a854d69712e76d901ce0a0325dae77f012ae
Phong Phan <fongfan999@gmail.com>
2017-03-25 18:58:41 +0700

    Added csrf_token

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
 require File.expand_path('../sneakersnstuff/config', __FILE__)
 
-user_cookie = 'DZdwFQTxaApjKVjliDbwZ30u56NUBr1Mh5LzGYss1GbelnMZ1riEkwd8pjZEqGDS3QNCPreibCubrWVC4RziIKRgNXNXH9tstxSVGCcWWfZ2Nab5'
+user_cookie = 'jgg6mW0iwLCHLVfGCzoXdjreiODAixC3fPTKrzBYRGz6AxYGxYwHmLjMiPT67TAn4z/7Jsq6XDvV5IGPFBjjHxaTGsUv1O4u6FDNIvV5GfcFhnNp'
+csrf_token = '6fb65cb4164f448fa0d8198f7778a78b'
+
 product_id = '183023'
 
 sstuff = Tap::V2::Sneakersnstuff.new(user_cookie)
-sstuff.add_to_shipping_cart(product_id)
+sstuff.add_to_shipping_cart(product_id, csrf_token)
--- sneakersnstuff/v2/sneakersnstuff.rb
+++ sneakersnstuff/v2/sneakersnstuff.rb
@@ -1,7 +1,7 @@
 module Tap
   module V2
     class Sneakersnstuff < Tap::Sneakersnstuff::Base
-      def add_to_shipping_cart(product_id)
+      def add_to_shipping_cart(product_id, csrf_token)
         agent = Mechanize.new
         agent.user_agent_alias = 'Mac Safari'
         agent.cookie_jar << Mechanize::Cookie.new(
@@ -9,7 +9,7 @@ module Tap
           value: @user_cookie)
 
         agent.post('http://www.sneakersnstuff.com/en/cart/add', [
-            %w(_AntiCsrfToken c8d31986fae34ee2a7805ce5c1c63456),
+            %W(_AntiCsrfToken #{csrf_token}),
             %w(partial cart-summary),
             %W(id #{product_id})
           ]
